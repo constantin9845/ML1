fde88cf7ad1e34d8e9b5ebfd1e6419fb961d0fa8
constantin.y@outlook.com <constantin.y@outlook.com>
2025-11-22 16:38:35 +0900

    added parallel execution for training functions

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 import model
 import numpy as np
 import os
+import time
+from multiprocessing import Process
 
 def get_data(type, hidden_states):
     folder = f'augmented_data_gaussian/{type}'
@@ -68,50 +70,63 @@ def get_data(type, hidden_states):
     return model.HMM(hidden_states, type), train_sequences, test_sequences
 
 
-def train(models):
-    for model in models:
-        model[0].train(model[1])
-        print("Finished training: ", model[0].get_label())
+def train(model, set):
+    model.train(set)
+    print("Finished training: ", model.get_label())
 
 def test(models, sequence, true_label):
-    max = 0
+    best = -np.inf
     label = ''
-    for i in range(len(models)):
-        
-        res = models[i].classify(sequence)
-
-        if i == 0:
-            max = res
-            label = models[0].get_label()
-        else:
-            if res > max:
-                max = res
-                label = models[i].get_label()
-
+    
+    for model in models:
+        score = model.classify(sequence)
+        if score > best:
+            best = score
+            label = model.get_label()
 
     return true_label == label
 
     
 
-HMM_circle, circle_training_set, circle_test_set = get_data('circle', 6)
-HMM_diagonal_left, diagonal_left_training_set, diagonal_left_test_set = get_data('diagonal_left', 6)
-HMM_diagonal_right, diagonal_right_training_set, diagonal_right_test_set = get_data('diagonal_right', 6)
-HMM_horizontal, horizontal_training_set, horizontal_test_set = get_data('horizontal', 6)
-HMM_vertical, vertical_training_set, vertical_test_set = get_data('vertical', 6)
+if __name__ == "__main__":
+    HMM_circle, circle_training_set, circle_test_set = get_data('circle', 4)
+    HMM_diagonal_left, diagonal_left_training_set, diagonal_left_test_set = get_data('diagonal_left', 4)
+    HMM_diagonal_right, diagonal_right_training_set, diagonal_right_test_set = get_data('diagonal_right', 4)
+    HMM_horizontal, horizontal_training_set, horizontal_test_set = get_data('horizontal', 4)
+    HMM_vertical, vertical_training_set, vertical_test_set = get_data('vertical', 4)
 
-model_set = [HMM_circle, HMM_diagonal_left, HMM_diagonal_right, HMM_horizontal, HMM_vertical]
-test_set = [circle_test_set+diagonal_left_test_set+diagonal_right_test_set+horizontal_test_set+vertical_test_set]
-
-train([[HMM_circle, circle_training_set], [HMM_diagonal_left, diagonal_left_training_set], [HMM_diagonal_right, diagonal_right_training_set], [HMM_horizontal, horizontal_training_set], [HMM_vertical, vertical_training_set]])
+    model_set = [HMM_circle, HMM_diagonal_left, HMM_diagonal_right, HMM_horizontal, HMM_vertical]
+    test_set = circle_test_set+diagonal_left_test_set+diagonal_right_test_set+horizontal_test_set+vertical_test_set
 
 
-correct = 0
-for test_sequences in test_set:
-    
-    for sequence in test_sequences:
+
+    processes = [
+        Process(target=train, args=(HMM_circle, circle_training_set)),
+        Process(target=train, args=(HMM_diagonal_left, diagonal_left_training_set)),
+        Process(target=train, args=(HMM_diagonal_right, diagonal_right_training_set)),
+        Process(target=train, args=(HMM_horizontal, horizontal_training_set)),
+        Process(target=train, args=(HMM_vertical, vertical_training_set)),
+    ]
+
+
+    start = time.time()
+
+    for p in processes:
+        p.start()
+
+    for p in processes:
+        p.join()
+
+    end = time.time()
+
+
+    correct = 0
+    for sequence in test_set:
         
-        if(test(model_set, np.array(sequence[0]), sequence[1])):
+        if test(model_set, np.array(sequence[0]), sequence[1]):
             correct += 1
 
-print("Accuracy : ",correct/len(test_set),"%")
+    accuracy = correct/len(test_set)*100
+    print("Accuracy : ",accuracy,"%")
+    print("Training time: ",(end-start),"s")
 
